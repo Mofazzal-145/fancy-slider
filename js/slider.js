e3a3444be10e33307451eb438e7f2f4023ec4197
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-12 20:02:56 +0600

    slider part

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+
+const images = [
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/7.jpg',
+    'images/8.jpg',
+    'images/9.jpg',
+    'images/10.jpg',
+    'images/11.jpg',
+    'images/12.jpg'
+];
+
+let imgIndex = 0;
+const imgElement = document.getElementById('slider-img');
+setInterval(() => {
+    if(imgIndex >= images.length){
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    imgElement.setAttribute('src', imgUrl);
+    imgIndex++;
+},1000);
